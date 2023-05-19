3dc59391982291ca5f3e0d79a8a17f099519f728
liuyi.hollow <liuyi.hollow@bytedance.com>
2023-05-19 18:46:59 +0800

    feat: 调整侧边栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,63 +3,64 @@ module.exports = {
   lang: 'zh-CN',
   title: 'MySelf',
   head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
-  base:'/myselfBlog/',
+  base: '/myselfBlog/',
   themeConfig: {
     sidebarDepth: 2,
-    home:false,
+    lastUpdated:'上次更新',
+    home: false,
     nav: [
       { text: 'Home', link: '/home/' },
-      { text: '前端', link: '/web/', items: [
-        {text:'事件循环',link:'/web/事件循环'},
-      ]},
+      {
+        text: '前端', items: [
+          {
+            text: 'JavaScript',
+            link: '/JavaScript/',
+          },
+          {
+            text: 'ES6',
+            link: '/ES6/',
+          },
+        ]
+      },
       { text: 'GitHub', link: 'https://github.com/l-1999-y/myselfBlog', target: '_blank' },
     ],
-    sidebar: 'auto',
-    // sidebar: [
-    //   {
-    //     title: '首页',
-    //     path: '/home/',
-    //     collapsable:false
-    //   },
-    //   {
-    //     title: '前端',   // 必要的
-    //     path: '/web/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-    //     collapsable: true, // 可选的, 默认值是 true,
-    //     sidebarDepth: 2,    // 可选的, 默认值是 1
-    //     children: [
-    //       {
-    //         title: '原理—事件循环',
-    //         path: '/web/事件循环',
-    //         children: [
-    //           {
-    //             title: '事件循环',
-    //             path: '/web/1',
-    //           }
-    //         ]
-    //       },
-    //       {
-    //         title: 'two',
-    //         path: '/web/two',
-    //       }
-    //     ]
-    //   },
-    //   // {
-    //   //   title: 'bar',
-    //   //   path: '/bar/',
-    //   //   children: [
-    //   //     {
-    //   //       title: 'three',
-    //   //       path: '/bar/three',
-    //   //     },
-    //   //     {
-    //   //       title: 'four',
-    //   //       path: '/bar/four',
-    //   //     }
-    //   //   ]
-    //   // }
-    // ],
+   
+    sidebar: {
+      '/JavaScript/': [
+        {
+          title: 'JavaScript',
+          collapsible: true,
+          children: [
+            {title:'简介', path:'/JavaScript/'},
+            '/JavaScript/防抖',
+            '/JavaScript/节流',
+          ],
+        }
+      ],
+      '/ES6/': [
+        {
+          title: 'ES6',
+          collapsible: true,
+          children: [
+            {title:'简介', path:'/ES6/'},
+            // '/ES6/防抖',
+            // '/ES6/节流',
+          ],
+        }
+      ],
+      '/home/': [{
+        title: '首页',
+        path: '/home/',
+        collapsable: false
+      }],
+    },
+
     search: true,
+
     searchMaxSuggestions: 10,
     lastUpdate: '上次更新',
+  },
+  markdown: {
+    lineNumbers: true
   }
 }
